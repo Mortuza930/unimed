356f3410dd1ab7944daac2b66b4dfa5af9803fcb
Mortuza930 <mortuza.unigroup@gmail.com>
2025-10-24 22:52:17 +0600

    Update settings for Render deployment

--- unimed/settings.py
+++ unimed/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bp5qihplic)2v^7adkbhug!=vbnr1!$^bi4z-f7kh6_6+q(#w%'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
